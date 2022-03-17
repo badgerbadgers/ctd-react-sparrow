44b0cf8b25a17033430e362784335a3e609c9e22
Tim <tim.almacen@gmail.com>
2022-03-16 22:37:12 -0700

    code for lesson-1-3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function AddTodoForm() {
+/* a functional component that takes in props */
+function AddTodoForm(props) {
+    /*  */
+    const [todoTitle, setTodoTitle] = useState('');
+  /* 
+   a handle todo function that takes an event as a parameter, runs event
+   preventDefault, console.logs(todoTitle) and sets the value todoTitle 
+   to an empty string
+  */
+  function handleTodo(event) {
+      {/* 
+       -- I was confused by the line below so I passed in todoTitle instead.
+       As I understand it I am invoking the callback function passed down
+       as a prop and passing in the todoTitle which as the current value
+       from the input field, this will then become the newTodo value. --
+
+      [] Inside the handleAddTodo function, invoke the onAddTodo callback
+       prop and pass newTodo as an argument 
+    */}
+      props.onAddTodo(todoTitle)
+      event.preventDefault();
+      console.log(todoTitle)
+      setTodoTitle('');
+  }
+
+  /* 
+    returns a form that contains a label element, a text input with 3 attributes
+    value that takes the value of the todoTitle variable, name and onChange
+    onChange runs an anonymous function that takes in an event and invokes the
+    setTodoTitle and set a new value for todoTitle 
+    */
   return (
     <div>
-      <form>
+      {/* onSubmit form will run handleTodo */}
+      <form onSubmit={handleTodo}>
           <label htmlFor="todoTitle">Title</label>
-          <input id="todoTitle"></input>
+          <input id="todoTitle" type="text" value={todoTitle} name="title" onChange={(e) => setTodoTitle(e.target.value)}></input>
           <button>Add</button>
       </form>
     </div>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoListItem from './TodoListItem';
 
 /* an array with 3 objects that have 2 properties: id and title */
 const todoList = [
@@ -13,27 +14,29 @@ const todoList = [
       id:3,
       title:"third"
     }
-  ]
+  ];
 
 /* 
   a functional component that maps through an array of data
   and renders specific values to back to the DOM
 */
 function TodoList() {
+
   return(
     <>
       <ul>
-      {/* maps over the array, the key attribute uses the object id */}
+      {/* maps over the array, the key attribute uses the object id
+        react does not let you use key prop that is passed down
+        you must use another prop attribute
+      */}
       {todoList.map((item) => {
         return(
-          <li key={item.id}>
-            <p>{item.title}</p>
-          </li>
+            <TodoListItem id={item.id} key={item.id} todo={item} />
         )
       })}
     </ul>
-        </>
-    )
+    </>
+  )
 }
 
 export default TodoList;

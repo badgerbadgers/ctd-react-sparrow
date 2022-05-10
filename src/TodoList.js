bacffc52550b015986d696d27bc4a8d6852b1f11
Tim <tim.almacen@gmail.com>
2022-05-10 16:34:39 -0700

    added new features sorting by time and sorting by date

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
 import TodoListItem from './TodoListItem'
-import PropTypes from 'prop-types'
-import { FaSort } from 'react-icons/fa'
+// import PropTypes from 'prop-types'
+import { FaSort, FaClock } from 'react-icons/fa'
 import style from './TodoListItem.module.css'
-
 
 /*
  a functional component that takes in props and maps through that data
@@ -12,16 +11,18 @@ import style from './TodoListItem.module.css'
 */
 const TodoList = ({ todoList, onRemoveTodo }) => {
   const [isAscending, setIsAscending] = useState(true)
-  const [updatedTodoList, setUpdatedTodoList] = useState([])
-  
-  function sort(a, b) {
+  // const [updatedTodoList, setUpdatedTodoList] = useState([])
+  const [formattedTodos, setFormattedTodos] = useState([])
+
+
+  function titleSort(a, b) {
     /* sort in ascending order */
     if(!isAscending) {
       setIsAscending(!isAscending)
-      if(a.fields.Name < b.fields.Name) {
+      if(a.title < b.title) {
         return -1
       }
-      if(a.fields.Name > b.fields.Name) {
+      if(a.title > b.title) {
         return 1
       }
       return 0
@@ -29,10 +30,35 @@ const TodoList = ({ todoList, onRemoveTodo }) => {
     /* sort in descending order */
     if (isAscending) {
       setIsAscending(!isAscending)
-      if(a.fields.Name < b.fields.Name) {
+      if(a.title < b.title) {
         return 1
       }
-      if(a.fields.Name > b.fields.Name) {
+      if(a.title > b.title) {
+        return -1
+      }
+      return 0
+    }
+  }
+  
+  function timeSort(a, b) {
+    /* sort in ascending order */
+    if(!isAscending) {
+      setIsAscending(!isAscending)
+      if(a.time < b.time) {
+        return -1
+      }
+      if(a.time > b.time) {
+        return 1
+      }
+      return 0
+    }
+    /* sort in descending order */
+    if (isAscending) {
+      setIsAscending(!isAscending)
+      if(a.time < b.time) {
+        return 1
+      }
+      if(a.time > b.time) {
         return -1
       }
       return 0
@@ -40,38 +66,59 @@ const TodoList = ({ todoList, onRemoveTodo }) => {
   }
 
   const handleSort = (order) => {
-    let sorted = todoList.sort(order)
-    setUpdatedTodoList(sorted)
-  };
+    console.log('formatted',formattedTodoList)
+    const updatedTodos = formattedTodoList.sort(timeSort)
+    setFormattedTodos(updatedTodos)
+  }
+
+   const formattedTodoList = 
+      todoList.map((item) => {
+        const currentDate = item.createdTime.split('T')
+        const date = currentDate[0]
+        const time = currentDate[1]
+        const title = item.fields.Name
+        const id = item.id
+        const todo = {
+          title: title,
+          id: id,
+          date: date,
+          time: time
+        }
+        return todo
+      })
+  
+    // setFormattedTodos(formattedTodoList)
+    // console.log('formatted list', formattedTodoList)
+    // const sortedDate = date.sort((a,b) => b - a)
+    // console.log(sortedDate)
 
   return(
     <>
       <ul>
-      <FaSort onClick={() => handleSort(sort)} className={style.sortBtn} />
-        {updatedTodoList.length === 0 ?
-        todoList.map((item) => {
-        return(
-          <div key={item.id}>
-            <TodoListItem 
-            id={item.id} 
-            key={item.id} 
-            todo={item} 
-            onRemoveTodo={onRemoveTodo}
-            />
-          </div>
-        )
-        }) : updatedTodoList.map((item) => {
-        return(
-          <div key={item.id}>
-            <TodoListItem 
-            id={item.id} 
-            key={item.id} 
-            todo={item} 
-            onRemoveTodo={onRemoveTodo}
-            />
-          </div>
-        )
-        })}
+        <p>
+          <FaSort onClick={() => handleSort(titleSort)} className={style.sortBtn} />
+          <FaClock onClick={() => handleSort(timeSort)} className={style.timeBtn} />
+        </p>
+      {formattedTodos.length === 0 ?
+    todoList.map((item) => {
+    return( // mapping default list 
+      <div key={item.id}>
+        <TodoListItem  
+        todo={item} 
+        onRemoveTodo={onRemoveTodo}
+        />
+      </div>
+    )
+    }) : formattedTodos.map((item) => {
+    return( //mapping sorted list 
+      <div key={item.id}>
+        <TodoListItem 
+        todo={item} 
+        onRemoveTodo={onRemoveTodo}
+        />
+      </div>
+    )
+    })}
       </ul>
     </>
   )
@@ -84,7 +131,3 @@ imported PropTypes and created an object that has properties that have a key equ
 to the prop or function being test and the value that uses PropTypes to test if output 
 is type that is written (object, string, etc)
 */
-TodoList.propTypes = {
-  todoList: PropTypes.array,
-  onRemoveTodo: PropTypes.func,
-}
